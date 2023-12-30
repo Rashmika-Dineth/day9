048120da5e73966decae12158b4dafd0d477c2e8
Rashmika <rashmika@atera.io>
2023-12-30 14:38:07 +0530

    indent update

--- Gallery/gallery.php
+++ Gallery/gallery.php
@@ -16,31 +16,24 @@ $result = $conn->query($sql);
 </head>
 
 <body>
-
     <div class="container mt-3">
         <h2>User Table</h2>
-
-              <?php
-	if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {?>
-          
-        <div class="card text-white bg-dark mb-3" style="width: 12rem;">
-            <img src="../Assets/Images/<?php echo $row["photo"]?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $row["title"] ?></h5>
-                <p class="card-text"><?php echo $row["description"] ?> </p>
-                <a href="/day9/Gallery/item?<?php echo $row["id"] ?>" class="btn btn-primary">Go somewhere</a>
-        </div>
+        <?php
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {?>
+                    <div class="card text-white bg-dark mb-3" style="width: 12rem;">
+                        <img src="../Assets/Images/<?php echo $row["photo"]?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row["title"] ?></h5>
+                            <p class="card-text"><?php echo $row["description"] ?> </p>
+                            <a href="/day9/Gallery/item?<?php echo $row["id"] ?>" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+        <?php } } ?>
     </div>
- 
-<?php } } ?>
-
-    </div>
-
 </body>
 
 </html>
 <?php
 include __DIR__."/../Footer/footer.html";
-
 ?>
